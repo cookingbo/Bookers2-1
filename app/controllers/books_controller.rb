45a9097d19c5dff5b0023983736f245ddef2e133
EC2 Default User <ec2-user@ip-172-31-29-17.ec2.internal>
2022-05-04 13:18:32 +0000

    [update]更新時にエラー、サクセスメッセージ出ない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = 'You have created book successfully.'
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render:index
+    end
   end
 
   def index
@@ -23,12 +28,17 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to books_path
+    if @book.update(book_params)
+      flash[:notice] = 'You have updated book successfully.'
+      redirect_to book_path(@book.id)
+    else
+      render:edit
+    end
   end
 
   def destroy
