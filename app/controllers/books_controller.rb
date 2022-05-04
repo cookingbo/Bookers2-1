1e5d926c1878f9b8cfffcbe56733347941ee2fcc
EC2 Default User <ec2-user@ip-172-31-29-17.ec2.internal>
2022-05-04 09:10:43 +0000

    [update]bookのshowのif文から

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @book = Book.new
   end
@@ -7,11 +9,12 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book.id)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -19,6 +22,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
 
